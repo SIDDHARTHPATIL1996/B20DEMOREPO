97c01f2ee7d0cd006bfe22654e54093d69857598
Siddharth <1996siddharthspatil@gmail.com>
2023-06-17 15:03:45 +0530

    Calculator class added

--- DemoSolution/First/Program.cs
+++ DemoSolution/First/Program.cs
@@ -1,7 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using First;
+
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine("Please enter first no. :");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter Second no. :");
+int b = int.Parse(Console.ReadLine());
 
-Console.WriteLine("fullstack developer");
-Console.WriteLine("Made changes");
+
+Calculator calculator = new Calculator();
+calculator.Add(a,b);
+
 Console.ReadLine();
